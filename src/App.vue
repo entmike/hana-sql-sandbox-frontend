4b689ece0230539f0b8fd3d29d9037d95a8a7b93
Mike <entmike@gmail.com>
2019-04-25 12:11:03 -0500

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-app id="inspire">
+      <div class="codemirror">
+        <codemirror v-model="code"/>
+      </div>
+      <v-btn @click="getData" color="success" >Run</v-btn>
+      <v-data-table
+        :headers="headers"
+        :items="desserts"
+        :loading="loading"
+        :rows-per-page="rowsPerPage"
+        class="elevation-1">
+        <template v-slot:items="props">
+          <td v-for="field of props.item">
+            {{ field }}
+          </td>
+        </template>
+      </v-data-table>
+    </v-app>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  import axios from 'axios';
+  import codemirror from 'vue-codemirror'
+  import 'codemirror/mode/sql/sql.js'
+  import 'codemirror/theme/solarized.css'
+  import 'codemirror/theme/base16-dark.css'
+ 
+  export default {
+    data: () => ({
+      headers: [],
+      desserts: [],
+      rowsPerPage : "-1",
+      loading : false,
+      code : `-- SQL Statement
+SELECT SCHEMA_NAME, TABLE_NAME, RECORD_COUNT FROM M_TABLES ORDER BY RECORD_COUNT DESC
+`
+    }),
+    methods: {
+      getData: function(e){
+        axios.post("http://localhost:9999/api/top-tables/",{
+          sql : this.code
+        }).then(res=>{
+          this.loading=false;
+          this.headers = [];
+          if(res.data && res.data.length>0){
+            let row = res.data[0];
+            for(var field in row) this.headers.push({text: field, value: field});
+          }
+          this.desserts = res.data;
+        }, err=> {
+          this.loading = false;
+          alert(JSON.stringify(err.response.data));
+        }).catch(err=>{
+          
+        })
+      }
+    },
+    mounted(){
+      this.getData();
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.CodeMirror {
+  height: 150px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+import VueCodemirror from 'vue-codemirror'
+
+// require styles
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/mode/javascript/javascript.js'
 
 Vue.config.productionTip = false
+
+Vue.use(VueCodemirror, { 
+  options: { 
+    tabSize: 4,
+    styleActiveLine: true,
+    lineNumbers: true,
+    line: true,
+    mode: 'text/x-mysql',
+    // theme: 'solarized light'
+    theme: 'base16-dark'
+  },
+  events: ['scroll']
+})
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
